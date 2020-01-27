a4c0cac52f790584a9d4a1a22c68cbde82a50f03
Wanjiku Karanja <ciiku92@gmail.com>
2020-01-27 03:15:58 +0300

    Use __init__ method to define the properties of user object

--- user.py
+++ user.py
@@ -2,6 +2,16 @@ class User:
     """
     Class that shall generate new instances of user accounts.
     """
-    
+
     #User Details Array 
     user_details = []
+
+    def __init__(self,account_name,username,user_password,confirm_password):
+        """
+        This __init__ method defines the properties of the User object
+        """
+
+        self.account_name = account_name
+        self.username = username
+        self.user_password = user_password
+        self.confirm_password = confirm_password
