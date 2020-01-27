ec67436bfd2aa8b122c4f9b8e507f0139937c3f2
Wanjiku Karanja <ciiku92@gmail.com>
2020-01-27 06:32:38 +0300

    Add a method that adds new credentials to the credential_details array

--- credentials.py
+++ credentials.py
@@ -4,3 +4,27 @@ class Credential:
     """
     #Credentials details array
     credential_details = []
+
+    def __init__(self,fname,cred_account,cred_username,cred_password):
+        """
+        This __init__ method defines the properties of the Credentials object
+
+        Args:
+            fname : the user's real first name
+            cred_account: name of new credential
+            cred_username : username of the credential account
+            cred_password : password of the crential account
+
+        """
+
+        self.fname = fname
+        self.cred_account = cred_account
+        self.cred_username = cred_username
+        self.cred_password = cred_password
+
+    def save_new_credential(self):
+        """
+        This is a method that saves a new credential to the credential_details array
+        """
+
+        Credential.credential_details.append(self)
