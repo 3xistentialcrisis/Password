21584afe54ced0a102d06774cc9cb27f511e02a3
Wanjiku Karanja <ciiku92@gmail.com>
2020-01-27 03:29:40 +0300

    Create run.py and add function that creates the new_user account

---
+++ run.py
@@ -0,0 +1,17 @@
+from user import User
+
+def create_user_account(account_name,username,password,confirm_password):
+    """
+    This function creates the user account
+
+    Args:
+        account_name : This is the platform on which the user creates their account
+        username : This is the user's name on the platform/account
+        password : This is the password chosen by the user
+        confirm_password : This confirms the user's password
+    """
+
+    new_user = User(account_name,username,password,confirm_password) 
+    
+    return new_user
+    
