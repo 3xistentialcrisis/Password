0f68a660c5684ef8e4bdeb3f1782c4afdde86e2b
Wanjiku Karanja <ciiku92@gmail.com>
2020-01-27 04:04:55 +0300

    Add save_user test case to test if user object is saved in user_details array

--- user-test.py
+++ user-test.py
@@ -1,2 +1,49 @@
 import unittest
-from user import User 
+from user import User
+
+class TestUser(unittest.TestCase):
+    """
+    Test Class which defines test cases for the User Class Behaviours
+
+    Args:
+        unittest.TestCase : Test case class used to create test cases
+    """
+
+    def setUp(self):
+        """
+        This setUp method runs before each test case
+        """
+
+        #User Object
+        self.new_user = User("instagram","mary","cat","cat") 
+
+        #User Details Array
+        User.user_details = []
+
+    def tearDown(self):
+        """
+        This tearDown method cleans up after each test case is run
+        """
+
+    #Test Cases 
+
+    def test_init(self):
+        """
+        This test case tests if the User object is initialised properly
+        """
+
+        self.assertEqual(self.new_user.account_name,"instagram")
+        self.assertEqual( self.new_user.username, "mary" )
+        self.assertEqual( self.new_user.password, "cat" )
+        self.assertEqual( self.new_user.confirm_password, "cat" )
+    
+    def test_save_user(self):
+        """
+        This test case tests if the user object is saved in the user_details array
+        """
+
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_details),1)
+
+if __name__ == '__main__':
+    unittest.main()
